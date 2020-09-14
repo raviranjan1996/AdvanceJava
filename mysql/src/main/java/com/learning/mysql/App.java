a94e7907d21858e356c6ec55ccb17b0563a61a10
raviranjan1996 <raviranjanp94@gmail.com>
2020-09-14 16:27:45 +0530

    unit test implemented

--- mysql/src/main/java/com/learning/mysql/App.java
+++ mysql/src/main/java/com/learning/mysql/App.java
@@ -2,27 +2,51 @@ package com.learning.mysql;
 
 import java.sql.SQLException;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-       
-        
-        var db = Database.instance();
-        try {
-        	db.connect();
-        }
-        catch(SQLException e) {
-        	System.out.println("Cannot connect to the db");
-        }
-        System.out.println("connected");
-        try {
+public class App {
+	public static void main(String[] args) {
+
+		var props = Profile.getProperties("db");
+		var db = Database.instance();
+		try {
+			db.connect(props);
+		} catch (SQLException e) {
+			System.out.println("Cannot connect to the db");
+			return;
+		}
+		
+		System.out.println("connected");
+
+		UserDao userDao = new UserDaoImpl();
+
+		// userDao.save(new User("Mark"));
+		// userDao.save(new User("Ranjan"));
+		// userDao.save(new User("Patel"));
+
+		var list = userDao.getAll();
+		list.forEach(e -> System.out.println(e));
+
+		var user = userDao.findById(12);
+
+		if (user.isPresent()) {
+
+			User users = user.get();
+			System.out.println("Retrieved " + users);
+			users.setName("snoopy");
+
+			userDao.update(users);
+
+		} else {
+			System.out.println("User Not present");
+		}
+
+		userDao.delete(new User(5, null));
+
+		try {
 			db.close();
 		} catch (SQLException e) {
 			System.out.println("cannot close the db");
 			e.printStackTrace();
 		}
-        
-        
-    }
+
+	}
 }
--- mysql/src/main/java/com/learning/mysql/Database.java
+++ mysql/src/main/java/com/learning/mysql/Database.java
@@ -3,27 +3,39 @@ package com.learning.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Database {
-		private static Database db = new Database();
-		private static final String URL = "jdbc:mysql://localhost:3306/people";
-		
-		private static Connection conn;
-		
-		public static Database instance() {
-			return db;
-		}
-		
-		private Database() {
-			
-		}
-		
-		public void connect() throws SQLException {
-			 conn = DriverManager.getConnection(URL , "root" , "e386c34b");
-		}
-		
-		public void close() throws SQLException {
-			conn.close();
-		}
-		
+	private static Database db = new Database();
+	// private static final String URL = "jdbc:mysql://localhost:3306/people";
+
+	private static Connection conn;
+
+	public static Database instance() {
+		return db;
+	}
+
+	private Database() {
+
+	}
+
+	public Connection getConnection() {
+		return conn;
+	}
+
+	public void connect(Properties props) throws SQLException {
+
+		String server = props.getProperty("server");
+		String port = props.getProperty("port");
+		String database = props.getProperty("database");
+		String user = props.getProperty("user");
+		String password = props.getProperty("password");
+		String url = String.format("jdbc:mysql://%s:%s/%s", server, port, database);
+		conn = DriverManager.getConnection(url, user, password);
+	}
+
+	public void close() throws SQLException {
+		conn.close();
+	}
+
 }
